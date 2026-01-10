65d87c1d5b3b68c220eb306f97b4ab0194af53e9
MizuAtusi <mizu.atusi@gmail.com>
2026-01-11 01:44:30 +0900

    じお

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const MOBILE_BREAKPOINT = 520;
+const MOBILE_BREAKPOINT = 1;
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
